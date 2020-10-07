7ad21422357f2ceed92d78bd4b4910f6573a9512
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-10-07 18:14:02 -0400

    initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        
+        /*
+            TODO - set initial state for link name and URL 
+        */
+
+        this.state ={
+            name: '',
+            url: ''
+        } 
+
+    }
+
+    handleChange = (event) => {
+        /*
+            TODO - Logic for changing state based on form changes
+        */
+
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        const url = target.url
+   
+        this.setState({
+            [name]: value,
+            [url] : value
+        });
+
+    } 
+
+    onFormSubmit = (event) => {
+        // to prevent page reload on form submit
+        event.preventDefault();
+        
+        /*
+            TODO - Logic for calling props to handle submission 
+            and setting state changes
+        */
+
+        this.setState({
+            name: "", 
+            url: ""
+        });
+
+        this.props.handleSubmit(this.state);
+
+    }
+
+    render() {
+
+        return(
+
+            <form>
+                {/* TODO - Logic for returning a form element 
+                with labels and inputs for link name and URL */}
+
+                <label for="name"> Name </label><br/>
+                <input type="text" id="name" name="name" value={this.state.name} onChange={this.handleChange}/><br/>
+                <label for="url"> URL </label><br/>
+                <input type="text" id="url" name="url" value={this.state.url} onChange={this.handleChange}/><br/>
+                
+                <input type="submit" value="Submit" onClick = {this.onFormSubmit}/> 
+
+            </form>
+        )
+    
+    }
+}
+
+export default Form;
